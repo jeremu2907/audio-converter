a8705420a207ea016e73a8a8c3b0906038f437b6
jeremu2907 <jeremu2907@gmail.com>
2024-05-20 03:11:01 -0500

    Update tailwind.config.js

--- app/tailwind.config.js
+++ app/tailwind.config.js
@@ -1,7 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,jsx}",
+        "./src/*.{js,jsx}",
     ],
     theme: {
         extend: {
