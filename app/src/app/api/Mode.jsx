b7693cf60f59554accdbf21b277e72985de61f1c
jeremu2907 <jeremu2907@gmail.com>
2024-05-21 01:14:21 -0500

    Update Mode.jsx

--- app/src/app/api/Mode.jsx
+++ app/src/app/api/Mode.jsx
@@ -6,22 +6,22 @@ import Mp3Converter from './Mp3Converter';
 export default function Mode() {
     const [mode, setMode] = useState(0);
 
-    const handleClick = (choice) => {
-        setMode(choice);
-    };
-
     return (
         <>
             <div className='w-max flex justify-evenly gap-10 border-b-2 border-gray-100 mb-10'>
-                <button role='button' onClick={() => handleClick(0)}
+                <button
+                    role='button'
+                    onClick={() => setMode(0)}
                     className={`border-b border-gray-200 py-2 mb-5 ${(mode === 0) && 'bg-blue-100'} rounded p-5 transition-all hover:bg-blue-100`}
                 >
                     .mp3 conversion
                 </button>
-                <button role='button'
+                <button
+                    role='button'
+                    onClick={() => setMode(1)}
                     className={`border-b border-gray-200 py-2 mb-5 ${(mode === 1) && 'bg-blue-100'} rounded p-5 transition-all hover:bg-blue-100`}
                 >
-                    more features coming soon ...
+                    batch .mp3 download
                 </button>
             </div>
             {(mode === 0) && <Mp3Converter />}
